bafbe2977302b5da28abc67ade2533f2a27c8697
David Waters <david@auror.co>
2023-09-10 15:40:45 +1200

    Add basic error handling and invoke command

--- SortByCamera.Cli/SortByCameraCli.cs
+++ SortByCamera.Cli/SortByCameraCli.cs
@@ -9,18 +9,28 @@ public class SortByCameraCli
 {
     public static void Main(string[] args)
     {
-        var rootCommand = new RootCommand
+        try
         {
-            new Option<DirectoryInfo>(name: "--source",
-                description: "The source directory to process images",
-                getDefaultValue: () => new DirectoryInfo(Directory.GetCurrentDirectory())
-            ).ExistingOnly()
-        };
-        
-        rootCommand.Description = "Sorts images into folders based on the camera they were taken with";
-        
-        rootCommand.Handler = CommandHandler.Create<DirectoryInfo>(SortByCamera);
-        
+            var rootCommand = new RootCommand
+            {
+                new Option<DirectoryInfo>(name: "--source",
+                    description: "The source directory to process images",
+                    getDefaultValue: () => new DirectoryInfo(Directory.GetCurrentDirectory())
+                ).ExistingOnly()
+            };
+
+            rootCommand.Description = "Sorts images into folders based on the camera they were taken with";
+
+            rootCommand.Handler = CommandHandler.Create<DirectoryInfo>(SortByCamera);
+            rootCommand.Invoke(args);
+        }catch(Exception ex)
+        {
+            Console.WriteLine("Well something went wrong, here is an unhelpful error message");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+            Console.WriteLine("Sorry that did not work, the above mess would tell a programmer what happened.");
+        }
+
     }
 
     static void SortByCamera(DirectoryInfo source)
